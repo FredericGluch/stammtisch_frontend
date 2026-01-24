cb934f8621b6a4dad45a5a3fef04ef17d2af2eac
FredericGluch <frederic.gluch@gmail.com>
2026-01-24 20:48:24 +0100

    package.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Rating } from '../rating/rating';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Rating}
+];
